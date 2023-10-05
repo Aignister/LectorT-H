26a7e42f464e10276ec7a5dab902c11833608ef9
Enrique Torres <enriquetorresv652@gmail.com>
2023-10-04 18:05:51 -0700

    Add project files.

---
+++ LectorT&H/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace LectorT_H
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            lblHumedad = new Label();
+            lblTemp = new Label();
+            groupBox1 = new GroupBox();
+            btnCerrar = new Button();
+            cmbBaud = new ComboBox();
+            btnAbrir = new Button();
+            cmbCom = new ComboBox();
+            label2 = new Label();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(297, 267);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(lblHumedad);
+            panel1.Controls.Add(lblTemp);
+            panel1.Location = new Point(315, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(338, 54);
+            panel1.TabIndex = 1;
+            // 
+            // lblHumedad
+            // 
+            lblHumedad.AutoSize = true;
+            lblHumedad.Location = new Point(221, 20);
+            lblHumedad.Name = "lblHumedad";
+            lblHumedad.Size = new Size(109, 15);
+            lblHumedad.TabIndex = 4;
+            lblHumedad.Text = "Humedad=___%RH";
+            // 
+            // lblTemp
+            // 
+            lblTemp.AutoSize = true;
+            lblTemp.Location = new Point(21, 20);
+            lblTemp.Name = "lblTemp";
+            lblTemp.Size = new Size(104, 15);
+            lblTemp.TabIndex = 3;
+            lblTemp.Text = "Temperatura=___C";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnCerrar);
+            groupBox1.Controls.Add(cmbBaud);
+            groupBox1.Controls.Add(btnAbrir);
+            groupBox1.Controls.Add(cmbCom);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 285);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(297, 100);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Ajustes Puerto COM";
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.Location = new Point(205, 57);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(75, 32);
+            btnCerrar.TabIndex = 4;
+            btnCerrar.Text = "Cerrar";
+            btnCerrar.UseVisualStyleBackColor = true;
+            btnCerrar.Click += btnCerrar_Click;
+            // 
+            // cmbBaud
+            // 
+            cmbBaud.FormattingEnabled = true;
+            cmbBaud.Location = new Point(85, 63);
+            cmbBaud.Name = "cmbBaud";
+            cmbBaud.Size = new Size(114, 23);
+            cmbBaud.TabIndex = 3;
+            // 
+            // btnAbrir
+            // 
+            btnAbrir.Location = new Point(205, 18);
+            btnAbrir.Name = "btnAbrir";
+            btnAbrir.Size = new Size(75, 33);
+            btnAbrir.TabIndex = 3;
+            btnAbrir.Text = "Abrir";
+            btnAbrir.UseVisualStyleBackColor = true;
+            btnAbrir.Click += btnAbrir_Click;
+            // 
+            // cmbCom
+            // 
+            cmbCom.FormattingEnabled = true;
+            cmbCom.Location = new Point(85, 24);
+            cmbCom.Name = "cmbCom";
+            cmbCom.Size = new Size(114, 23);
+            cmbCom.TabIndex = 4;
+            cmbCom.DropDown += cmbCom_DropDown;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 66);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 15);
+            label2.TabIndex = 3;
+            label2.Text = "BAUD RATE";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Puerto COM";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(666, 393);
+            Controls.Add(groupBox1);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private GroupBox groupBox1;
+        private Button btnCerrar;
+        private ComboBox cmbBaud;
+        private Button btnAbrir;
+        private ComboBox cmbCom;
+        private Label label2;
+        private Label label1;
+        private Label lblHumedad;
+        private Label lblTemp;
+    }
+}
---
+++ LectorT&H/Form1.cs
@@ -0,0 +1,126 @@
+using System;
+using System.IO.Ports;
+using static System.Windows.Forms.DataFormats;
+
+namespace LectorT_H
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double temperatura;
+        double humedad;
+        bool actDato = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnAbrir.Enabled = true;
+            btnCerrar.Enabled = false;
+
+            chart1.Series["Temperatura"].Points.AddXY(1, 1);
+            chart1.Series["Humedad"].Poitns.AddXY(1, 1);
+        }
+
+        private void cmbCom_DropDown(object sender, EventArgs e)
+        {
+            string[] portList = SerialPort.GetPortNames();
+            cmbCom.Items.AddRange(portList);
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = cmbCom.Text;
+                serialPort1.BaudRate = Convert.ToInt32(txtBaud.text);
+                serialPort1.Open();
+
+                btnAbrir.Enabled = false;
+                btnCerrar.Enabled = true;
+
+                chart1.Series["Temperatura"].Points.Clear();
+                chart1.Series["Humedad"].Points.Clear();
+
+                MessageBox.Show("Conexion exitosa a la tarjeta arduino");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();
+                btnAbrir.Enabled = true;
+                btnCerrar.Enabled = false;
+
+                MessageBox.Show("Desconectado de la tarjeta arduino");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Show_Data(object sender, EventArgs e)
+        {
+            if (actDato == true)
+            {
+                lblTemp.Text = string.Format("Temperatura= {0}°C",
+                temperatura.ToString());
+                lblHumedad.Text = string.Format("Humedad= {0}%HR",
+                humdad.ToString());
+                //Se dibujan los puntos en la gráfica
+                chart1.Series["Temperatura"].Points.Add(temperatura);
+                chart1.Series["Humedad"].Points.Add(humdad);
+            }
+        }
+
+        private void Data_Parsing(string dato)
+        {
+            sbyte indexOf_startDataCharacter = (sbyte)dato.IndexOf("@");
+            sbyte indexOfA = (sbyte)dato.IndexOf("A");
+            sbyte indexOfB = (sbyte)dato.IndexOf("B");
+
+            if(indexOfA != -1 && indexOfB != -1 && indexOf_startDataCharacter != -1)
+            {
+                try
+                {
+                    string str_Temperatura = dato.Substring(indexOf_startDataCharacter + 1, (indexOfA - indexOf_startDataCharacter) - 1);
+                    string str_Humedad = dato.Substring(indexOfA + 1, (indexOfB - indexOfA) - 1);
+
+                    temperatura = Convert.ToDouble(str_Temperatura);
+                    humedad = Convert.ToDouble(str_Humedad);
+                    actDato = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                actDato = false;
+            }
+        }
+    }
+}
